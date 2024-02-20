f10f5a37610e47244f3048181e3bf5129d4eacbd
AdityaRokade17 <rokadeaditya634@gmail.com>
2024-02-21 00:18:29 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,7 @@ var cors = require("cors");
 app.use(cors({
     origin: "*",
     credentials: true,
-    methods: ["GET" , "POST" , "PUT" , "DELETE"]
   }));
-  
-app.options("" , cors());
 
 require('dotenv').config();
 const PORT = process.env.PORT;
@@ -32,7 +29,9 @@ app.use("/api/v1" , eatsgpt);
 const moodmeal = require("./routes/moodMealsRoute");
 app.use("/api/v1/moodmeals" , moodmeal );
 
-
+app.get("/", (req, res) => {
+  res.send("Welcome to the root path!");
+});
 app.listen(PORT , () => {
     console.log(`server is running on ${PORT}`);
 
